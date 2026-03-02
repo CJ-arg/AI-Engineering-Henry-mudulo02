09449a26a55ffdf90f0f19c1806f4c23534989d3
Charly J <janoncarlos@gmail.com>
2026-03-02 15:44:05 -0300

    Flujo en Main

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from tp-modulo2-ai-henry!")
+import subprocess
+import sys
 
+def main():
+    print("Iniciando Proceso RAG TechFlow")
+    
+    print("Ejecutando: build_index.py")
+    subprocess.run([sys.executable, "src/build_index.py"])
+    
+    print("Ejecutando: query.py")
+    subprocess.run([sys.executable, "src/query.py"])
+
+    print("Proceso finalizado.")
 
 if __name__ == "__main__":
     main()
